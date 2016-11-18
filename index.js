9edc6ce6ccf061cc4c9aeb1ffd8f45e1cf80905f
Kevin Robinson <kevin.robinson.0@gmail.com>
2016-11-18 10:58:27 -0500

    Notifier app
    Add dependencies
    
    Env
    
    Add channel and name
    
    Start express
    
    Add bodyParser
    
    JSON body-parser
    
    Middleware logging
    
    Multer
    
    form encoding with body-parser
    
    Write to Slack manually
    
    Remove comma
    
    Accept anything
    
    Better message
    
    Simplify text
    
    Add error
    
    Simplify message further

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
-var SlackGun = require('node-slack-mailgun');
 var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var request = require('superagent');
+
+
+function tellSlack(slackMessage) {
+  var url = process.env.SLACK_WEBHOOK_URL;
+  if (!url) return console.log('Slack integration not enabled.');
+  request
+    .post(url)
+    .send(slackMessage)
+    .set('Accept', 'application/json')
+    .end();
+}
+
 
 var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer().any());
+app.use('/api/mailgun', function(req, res) {
+  console.log(req.headers);
+  console.log(Object.keys(req.body));
+  console.log(req.body);
 
-app.use('/api/mailgun', SlackGun({
-    slack: { hook: process.ENV.SLACK_WEBHOOK_URL },
-    mailgun: { apikey: process.ENV.MAILGUN_API_KEY }
-}));
+  // form message
+  var lines = [
+    'event: ' + req.body.event,
+    'recipient: ' + req.body.recipient
+  ];
+  ['code', 'description', 'reason', 'error'].forEach(function(key) {
+    if (req.body[key]) lines.push(key + ': ' + req.body[key]);  
+  });
+  var text = lines.join("\n");
+  
+  console.log(text);
+  tellSlack({
+    username: "forums-notifier",
+    icon_emoji: ":warning:",
+    text: text
+  });
+  res.json({
+    status: 'ok',
+    text: text
+  });
+});
+
+
+app.listen(process.env.PORT, function () {
+  console.log('Listening on port: ' + process.env.PORT);
+});
